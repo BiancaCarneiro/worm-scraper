7e85236bdc3ecad42c6a1fac0bebbf3300c943ee
BiancaCarneiro <bianca.ccnf@gmail.com>
2024-03-09 22:49:35 -0300

    Removing unecessary coments

--- create_epub.py
+++ create_epub.py
@@ -9,18 +9,16 @@ BOOKS_DIVIDERS = ["hive", "extermination", "plage", "colony", "queen", "imago", 
 def create_book(book_title:list, book_author:str, book_language:str, book_cover:str):
     crawling_bug = Skitter(TABLE_OF_CONTENTS)
     crawling_bug.get_links()
-    # chapters
+    
     for book_number in tqdm(range(NUM_BOOKS)):
         book = epub.EpubBook()
 
-        # add metadata
         book.set_identifier(book_title[book_number] + book_author)
         book.set_title(book_title[book_number])
         book.set_language(book_language)
 
         book.add_author(book_author)
 
-        # add cover image
         chapters = []
         links = []
         book.set_cover(book_cover[book_number], open(book_cover[book_number], 'rb').read())
@@ -35,21 +33,14 @@ def create_book(book_title:list, book_author:str, book_language:str, book_cover:
             chapter.content=content
 
             chapters.append(chapter)
-            # add chapters to the book
             book.add_item(chapter)
             links.append(epub.Link(_file_name, chapter_title, title_file))
-        # create table of contents
-        # - add manual link
-        # - add section
-        # - add auto created links to chapters
-
+            
         book.toc = (links)
 
-        # add navigation files
         book.add_item(epub.EpubNcx())
         book.add_item(epub.EpubNav())
 
-        # define css style
         style = '''
 @namespace epub "http://www.idpf.org/2007/ops";
 
@@ -83,14 +74,11 @@ nav[epub|type~='toc'] > ol > li > ol > li {
 
 '''
 
-        # add css file
         nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
         book.add_item(nav_css)
 
-        # create spin, add cover page as first page
         book.spine = ['cover', 'nav'] + chapters
 
-        # create epub file
         epub.write_epub(f'{book_title[book_number]}.epub', book, {})
     
 if __name__ == '__main__':
